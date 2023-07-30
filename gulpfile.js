e93e7034aedc9f5d678b97843100f8106b3fec88
D8bit <deibit@protonmail.com>
2023-07-30 20:09:05 +0200

    Updated all packages

--- gulpfile.js
+++ gulpfile.js
@@ -10,58 +10,74 @@ var gulp = require('gulp'),
     autoprefixer = require('gulp-autoprefixer');
 
 
-var autoprefixerOptions = {
-    browsers: ['last 5 versions', '> 1%', 'Firefox ESR']
-};
-
 gulp.task('move-files', function(){
-    // copy vendor folder
-    gulp.src(['src/vendor/**/*']).pipe(gulp.dest('dist/vendor'));
-    // copy img folder
-    gulp.src(['src/img/**/*']).pipe(gulp.dest('dist/img'));
-    // copy fonts folder
-    gulp.src(['src/fonts/**/*']).pipe(gulp.dest('dist/fonts'));
-    // copy other files
-    gulp.src(['src/index.html', 'src/.htaccess', 'src/robots.txt', 'src/sitemap.xml']).pipe(gulp.dest('dist/'));
+    return new Promise(function(resolve, reject) {
+        // copy vendor folder
+        gulp.src(['src/vendor/**/*']).pipe(gulp.dest('dist/vendor'));
+        // copy img folder
+        gulp.src(['src/img/**/*']).pipe(gulp.dest('dist/img'));
+        // copy fonts folder
+        gulp.src(['src/fonts/**/*']).pipe(gulp.dest('dist/fonts'));
+        // copy other files
+        gulp.src(['src/index.html', 'src/.htaccess', 'src/robots.txt', 'src/sitemap.xml']).pipe(gulp.dest('dist/'));
+        resolve();
+    })
 });
 
 gulp.task('minify-css', function() {
-    return gulp.src('src/**/*.css')
-        .pipe(sourcemap.init())
+    return new Promise(function(resolve, reject) {
+        gulp.src('src/**/*.css')
+            .pipe(sourcemap.init())
             .pipe(purify(['src/vendor/**/*.js', 'src/js/*.js', 'src/*.html']))
-            .pipe(cleanCSS({compatibility: 'ie8'}))
-            .pipe(autoprefixer(autoprefixerOptions))
+            .pipe(cleanCSS({ compatibility: 'ie8' }))
+            .pipe(autoprefixer())
             .pipe(concat('style.min.css'))
-        .pipe(sourcemap.write('maps'))
-        .pipe(gulp.dest('dist/css'));
+            .pipe(sourcemap.write('maps'))
+            .pipe(gulp.dest('dist/css'));
+        resolve();
+    });
 });
 
-gulp.task('purify-css', function() {
-    return gulp.src('dist/css/*.css')
-        .pipe(purify(['src/js/*.js', 'src/*.html']))
-        .pipe(gulp.dest('dist/css'));
+gulp.task('purify-css', function () {
+    return new Promise(function (resolve, reject) {
+        gulp.src('dist/css/*.css')
+            .pipe(purify(['src/js/*.js', 'src/*.html']))
+            .pipe(gulp.dest('dist/css'));
+        resolve();
+    });
 
 });
 
-gulp.task('minify-html', function() {
-    return gulp.src('src/*.html')
-        .pipe(htmlmin({collapseWhitespace: true}))
-        .pipe(gulp.dest('dist'));
+// gulp.task('minify-html', function() {
+//     return gulp.src('src/*.html')
+//         .pipe(htmlmin({collapseWhitespace: true}))
+//         .pipe(gulp.dest('dist'));
 
+// });
+
+gulp.task('minify-html', function () {
+    return new Promise(function (resolve, reject) {
+        gulp.src('src/*.html')
+            .pipe(htmlmin({ collapseWhitespace: true }))
+            .pipe(gulp.dest('dist'));
+        resolve();
+    });
 });
 
-gulp.task('minify-js', function() {
-    gulp.src('src/js/*.js')
-        .pipe(minify({
-            ext:{
-                src:'-debug.js',
-                min:'.js'
-            },
-            exclude: ['tasks'],
-            ignoreFiles: ['.combo.js', '-min.js']
-        }))
-        .pipe(gulp.dest('dist/js'));
-
+gulp.task('minify-js', function () {
+    return new Promise(function (resolve, reject) {
+        gulp.src('src/js/*.js')
+            .pipe(minify({
+                ext: {
+                    src: '-debug.js',
+                    min: '.js'
+                },
+                exclude: ['tasks'],
+                ignoreFiles: ['.combo.js', '-min.js']
+            }))
+            .pipe(gulp.dest('dist/js'));
+        resolve();
+    });
 });
 
 gulp.task('livereload', function() {
